feb531a9ece16c0376c023c5ba60c73b465db26c
nakanishikeita03 <0816724keita@gmail.com>
2019-10-22 16:35:32 +0900

    投稿削除機能

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :post,optional: true
 
-  enum language: { "html": 1,"css": 2, "javascript": 3, "rails": 4}, _prefix: true
+  enum language: { "html": 1,"css": 2, "javascript": 3, "ruby": 4, "php":5, "python":6, "java":7,"perl":8, "swift":9,"golang":10, "c":11,"c++":12,"c#":13,"その他":20}, _prefix: true
   enum time: { "1時間": 1, "2時間": 2, "3時間": 3,"4時間": 4, "5時間": 5, "6時間": 6,"7時間": 7, "8時間": 8, "9時間": 9,"10時間": 10, "11時間": 11, "12時間": 12}, _prefix: true
 end
