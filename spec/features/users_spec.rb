07a3c68dd4f5c482dd855d6816b47f69f69ea19f
nakanishikeita03 <0816724keita@gmail.com>
2019-11-02 18:00:52 +0900

    SNS認証featureテスト

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,35 +1,43 @@
-# require "rails_helper"
+require "rails_helper"
 
-# describe "UserFeature" do
+describe "UserFeature" do
 
-#   describe "facebook連携でサインアップする" do
+  describe "facebook連携でサインアップする" do
 
-#     before do
-#       OmniAuth.config.mock_auth[:facebook] = nil
-#       Rails.application.env_config['omniauth.auth'] = facebook_mock
-#       visit root_path
-#       click_link "ログイン"
-#     end
+    before do
+      OmniAuth.config.mock_auth[:facebook] = nil
+      Rails.application.env_config['omniauth.auth'] = facebook_mock
+      visit root_path
+      click_link "ログイン"
+    end
 
-#     it "サインアップするとユーザーが増える" do
-#       expect{
-#         click_link "facebookでログイン"
-#         fill_in "self_introduction", with: 'こんにちは'
-#         fill_in "password",with: 12345678
-#         fill_in "password_confirmation",with: 12345678
-#         fill_in "fname", with: 'user.jpeg'
-#         find('input[type="submit"]').click
-#       }.to change(User, :count).by(1)
-#     end
+    it "サインアップするとユーザーが増える" do
+      expect{
+        click_link "facebookでログイン"
+        attach_file "file_photo", "spec/fixtures/user.jpg"
+        fill_in "user[self_introduction]", with: 'こんにちは'
+        fill_in "user[password]",with: 12345678
+        fill_in "user[password_confirmation]",with: 12345678
+        find('input[type="submit"]').click
+      }.to change(User, :count).by(1)
+    end
 
-#     it "すでに連携されたユーザーがサインアップしようとするとユーザーは増えない" do
-#       click_link "facebookでログイン"
-#       click_link "ログアウト"
-#       click_link "ログイン"
-#       expect{
-#         click_link "facebookでログイン"
-#       }.not_to change(User, :count)
-#     end
+    it "すでに連携されたユーザーがサインアップしようとするとユーザーは増えない" do
+        click_link "facebookでログイン"
+        attach_file "file_photo", "spec/fixtures/user.jpg"
+        fill_in "user[self_introduction]", with: 'こんにちは'
+        fill_in "user[password]",with: 12345678
+        fill_in "user[password_confirmation]",with: 12345678
+        find('input[type="submit"]').click
 
-#   end
-# end
+      expect{
+        find(".user-icon").click
+        find(".signout-icon").click
+        expect(current_path).to eq root_path
+        click_link "ログイン"
+        click_link "facebookでログイン"
+      }.not_to change(User, :count)
+    end
+
+  end
+end
